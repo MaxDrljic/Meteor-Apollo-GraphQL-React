fb315008fea15c7f8a6f812bf9dc2366b13ed026
MaxDrljic <max_spree@hotmail.com>
2018-01-24 22:15:24 +0100

    Established MongoDB with Meteor, added new record in db, deleted dummy data.

--- imports/api/resolutions/resolvers.js
+++ imports/api/resolutions/resolvers.js
@@ -1,16 +1,13 @@
+import Resolutions from './resolutions';
+
+/* Resolutions.insert({
+  name: 'Test Res',
+}); */
+
 export default {
   Query: {
     resolutions() {
-      return [
-        {
-          _id: 'Generic String',
-          name: 'Get stuff done!',
-        },
-        {
-          _id: 'Second Generic String',
-          name: 'Get stuff done for the second time!',
-        },
-      ];
+      return Resolutions.find({}).fetch();
     },
   },
 };
