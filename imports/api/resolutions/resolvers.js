32a4b77252e4696a9960bd82be5a55e81642813a
MaxDrljic <max_spree@hotmail.com>
2018-01-26 08:43:54 +0100

    Established authorization with apollo link

--- imports/api/resolutions/resolvers.js
+++ imports/api/resolutions/resolvers.js
@@ -2,8 +2,11 @@ import Resolutions from './resolutions';
 
 export default {
   Query: {
-    resolutions() {
-      return Resolutions.find({}).fetch();
+    resolutions(obj, args, { userId }) {
+      console.log(userId);
+      return Resolutions.find({
+        userId,
+      }).fetch();
     },
   },
 
