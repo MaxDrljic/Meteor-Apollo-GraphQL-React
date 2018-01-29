1b602d173f34ecea15a32954980b24decb4feb12
MaxDrljic <max_spree@hotmail.com>
2018-01-29 20:07:09 +0100

    Created Goals

--- imports/api/resolutions/resolvers.js
+++ imports/api/resolutions/resolvers.js
@@ -3,7 +3,6 @@ import Resolutions from './resolutions';
 export default {
   Query: {
     resolutions(obj, args, { userId }) {
-      console.log(userId);
       return Resolutions.find({
         userId,
       }).fetch();
@@ -12,7 +11,6 @@ export default {
 
   Mutation: {
     createResolution(obj, { name }, { userId }) {
-      console.log(name);
       const resolutionId = Resolutions.insert({
         name,
         userId,
